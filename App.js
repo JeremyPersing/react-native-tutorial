78fcc77c30f738563ddfa199f84f59731d9689a7
Jeremy Persing <jpersing1999@gmail.com>
2021-02-24 20:56:48 -0800

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+// SafeAreaView ensures there will be padding at the top
+import {
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+  Alert,
+  Button,
+  SafeAreaView,
+  Image,
+} from "react-native";
+// View -> UIView
 export default function App() {
+  /*
+  const handlePress = () => {
+    console.log("Text has been pressed");
+  };*/
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // View is like a div
+    <SafeAreaView style={styles.container}>
+      <Text>Hello React Native</Text>
+      <Button
+        color="dodgerblue"
+        title="Click Me"
+        onPress={() =>
+          Alert.alert("My Title", "My Message", [
+            { text: "yes", onPress: () => console.log("Yes") },
+            { text: "no", onPress: () => console.log("No") },
+          ])
+        }
+      />
+      <TouchableHighlight
+        onPress={() => {
+          console.log("Image tapped");
+        }}
+      >
+        <Image
+          blurRadius={1}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
+    </SafeAreaView>
   );
 }
 
+// Contains all the styles for the app
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, // Will grow flexibly to take up the entirety of the screen
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
