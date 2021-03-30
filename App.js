056b387bafb68c30f3c562b60311f61bfab9bdd9
Jeremy Persing <34422870+JeremyPersing@users.noreply.github.com>
2021-03-30 16:32:19 -0700

    Internet Connection procedures improvements

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { Text, View } from "react-native";
-import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
+
+import { useNetInfo } from "@react-native-community/netinfo";
 
 import AppNavigator from "./app/navigation/AppNavigator";
-import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
-import AppPopdown from "./app/components/AppPopdown";
+import OfflineNotice from "./app/components/OfflineNotice";
 
 export default function App() {
-  const netInfo = useNetInfo();
-  console.log(!netInfo.isInternetReachable);
   return (
-    <NavigationContainer theme={navigationTheme}>
-      {!netInfo.isInternetReachable ? (
-        <AppPopdown>No Internet Connection</AppPopdown>
-      ) : (
-        <View></View>
-      )}
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <OfflineNotice />
+      <NavigationContainer theme={navigationTheme}>
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   );
 }
